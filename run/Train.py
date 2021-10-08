f159602c43088b3fc39e6dc4bf4059760340c455
Taehun Kim <taehoon1018@postech.ac.kr>
2021-10-08 13:48:14 +0900

    train scirpt update

--- run/Train.py
+++ run/Train.py
@@ -4,69 +4,145 @@ import argparse
 import tqdm
 import sys
 
-from torch.autograd import Variable
+import cv2
+import torch.nn as nn
+import torch.distributed as dist
+
+from torch.optim import Adam, SGD
+from torch.cuda.amp import GradScaler, autocast
 
 filepath = os.path.split(__file__)[0]
 repopath = os.path.split(filepath)[0]
 sys.path.append(repopath)
 
-from lib import *
-from lib.optim import *
 from utils.dataloader import *
-from utils.utils import *
+from lib.optim import *
+from lib import *
 
 def _args():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--config', type=str, default='configs/xomcnet.yaml')
+    parser.add_argument('--config', type=str, default='configs/UACANet-L.yaml')
     parser.add_argument('--local_rank', type=int, default=-1)
     parser.add_argument('--verbose', action='store_true', default=False)
     parser.add_argument('--debug', action='store_true', default=False)
     return parser.parse_args()
 
+
 def train(opt, args):
     device_ids = os.environ["CUDA_VISIBLE_DEVICES"].split(',')
     device_num = len(device_ids)
-    model = eval(opt.Model.name)(opt.Model).cuda()
-    
-    image_root = os.path.join(opt.Train.train_path, 'images')
-    gt_root = os.path.join(opt.Train.train_path, 'masks')
 
-    train_dataset = PolypDataset(image_root, gt_root, opt.Train)
+    train_dataset = eval(opt.Train.Dataset.type)(
+        root=opt.Train.Dataset.root, transform_list=opt.Train.Dataset.transform_list)
+
+    if device_num > 1:
+        torch.cuda.set_device(args.local_rank)
+        dist.init_process_group(backend='nccl')
+        train_sampler = torch.utils.data.distributed.DistributedSampler(
+            train_dataset, shuffle=True)
+    else:
+        train_sampler = None
+
     train_loader = data.DataLoader(dataset=train_dataset,
-                                    batch_size=opt.Train.batchsize,
-                                    shuffle=opt.Train.shuffle,
-                                    num_workers=opt.Train.num_workers,
-                                    pin_memory=opt.Train.pin_memory)
+                                    batch_size=opt.Train.Dataloader.batch_size,
+                                    shuffle=train_sampler is None,
+                                    sampler=train_sampler,
+                                    num_workers=opt.Train.Dataloader.num_workers,
+                                    pin_memory=opt.Train.Dataloader.pin_memory,
+                                    drop_last=True)
 
-    params = model.parameters()
-    optimizer = torch.optim.Adam(params, opt.Train.lr)
-    scheduler = PolyLr(optimizer, gamma=opt.Train.gamma,
-                        minimum_lr=opt.Train.min_learning_rate,
-                        max_iteration=len(train_loader) * opt.Train.epoch,
-                        warmup_iteration=opt.Train.warmup_iteration)
+    model = eval(opt.Model.name)(channels=opt.Model.channels,
+                                 pretrained=opt.Model.pretrained)
+
+    if device_num > 1:
+        model = nn.SyncBatchNorm.convert_sync_batchnorm(model)
+        model = model.cuda()
+        model = nn.parallel.DistributedDataParallel(model, device_ids=[
+                                                    args.local_rank], output_device=args.local_rank, find_unused_parameters=True)
+    else:
+        model = model.cuda()
+
+    backbone_params = nn.ParameterList()
+    decoder_params = nn.ParameterList()
+
+    for name, param in model.named_parameters():
+        if 'backbone' in name:
+            if 'backbone.layer' in name:
+                backbone_params.append(param)
+            else:
+                pass
+        else:
+            decoder_params.append(param)
+
+    params_list = [{'params': backbone_params}, {
+        'params': decoder_params, 'lr': opt.Train.Optimizer.lr * 10}]
+    optimizer = eval(opt.Train.Optimizer.type)(
+        params_list, opt.Train.Optimizer.lr, weight_decay=opt.Train.Optimizer.weight_decay)
+    if opt.Train.Optimizer.mixed_precision is True:
+        scaler = GradScaler()
+    else:
+        scaler = None
+
+    scheduler = eval(opt.Train.Scheduler.type)(optimizer, gamma=opt.Train.Scheduler.gamma,
+                                               minimum_lr=opt.Train.Scheduler.minimum_lr,
+                                               max_iteration=len(
+                                                   train_loader) * opt.Train.Scheduler.epoch,
+                                               warmup_iteration=opt.Train.Scheduler.warmup_iteration)
     model.train()
 
-    print('#' * 20, 'Train prep done, start training', '#' * 20)
+    if args.local_rank <= 0 and args.verbose is True:
+        epoch_iter = tqdm.tqdm(range(1, opt.Train.Scheduler.epoch + 1), desc='Epoch', total=opt.Train.Scheduler.epoch,
+                               position=0, bar_format='{desc:<5.5}{percentage:3.0f}%|{bar:40}{r_bar}')
+    else:
+        epoch_iter = range(1, opt.Train.Scheduler.epoch + 1)
 
-    for epoch in tqdm.tqdm(range(1, opt.Train.epoch + 1), desc='Epoch', total=opt.Train.epoch, position=0, bar_format='{desc:<5.5}{percentage:3.0f}%|{bar:40}{r_bar}'):
-        pbar = tqdm.tqdm(enumerate(train_loader, start=1), desc='Iter', total=len(train_loader), position=1, leave=False, bar_format='{desc:<5.5}{percentage:3.0f}%|{bar:40}{r_bar}')
-        for i, sample in pbar:
+    for epoch in epoch_iter:
+        if args.local_rank <= 0 and args.verbose is True:
+            step_iter = tqdm.tqdm(enumerate(train_loader, start=1), desc='Iter', total=len(
+                train_loader), position=1, leave=False, bar_format='{desc:<5.5}{percentage:3.0f}%|{bar:40}{r_bar}')
+            if device_num > 1:
+                train_sampler.set_epoch(epoch)
+        else:
+            step_iter = enumerate(train_loader, start=1)
+
+        for i, sample in step_iter:
             optimizer.zero_grad()
-            images, gts = sample['image'], sample['gt']
-            images = images.cuda()
-            gts = gts.cuda()
-            out = model(images, gts)
-            out['loss'].backward()
-            clip_gradient(optimizer, opt.Train.clip)
-            optimizer.step()
-            scheduler.step()
-            pbar.set_postfix({'loss': out['loss'].item()})
+            if opt.Train.Optimizer.mixed_precision is True:
+                with autocast():
+                    sample = to_cuda(sample)
+                    out = model(sample)
 
-        os.makedirs(opt.Train.train_save, exist_ok=True)
-        if epoch % opt.Train.checkpoint_epoch == 0:
-            torch.save(model.state_dict(), os.path.join(opt.Train.train_save, 'latest.pth'))
+                    scaler.scale(out['loss']).backward()
+                    scaler.step(optimizer)
+                    scaler.update()
+                    scheduler.step()
+            else:
+                sample = to_cuda(sample)
+                out = model(sample)
+                out['loss'].backward()
+                optimizer.step()
+                scheduler.step()
 
-    print('#' * 20, 'Train done', '#' * 20)
+            if args.local_rank <= 0 and args.verbose is True:
+                step_iter.set_postfix({'loss': out['loss'].item()})
+
+        if args.local_rank <= 0:
+            os.makedirs(opt.Train.Checkpoint.checkpoint_dir, exist_ok=True)
+            os.makedirs(os.path.join(
+                opt.Train.Checkpoint.checkpoint_dir, 'debug'), exist_ok=True)
+            if epoch % opt.Train.Checkpoint.checkpoint_epoch == 0:
+                torch.save(model.module.state_dict() if device_num > 1 else model.state_dict(
+                ), os.path.join(opt.Train.Checkpoint.checkpoint_dir, 'latest.pth'))
+
+            if args.debug is True:
+                debout = debug_tile(out)
+                cv2.imwrite(os.path.join(
+                    opt.Train.Checkpoint.checkpoint_dir, 'debug', str(epoch) + '.png'), debout)
+
+    if args.local_rank <= 0:
+        torch.save(model.module.state_dict() if device_num > 1 else model.state_dict(
+        ), os.path.join(opt.Train.Checkpoint.checkpoint_dir, 'latest.pth'))
+
 
 if __name__ == '__main__':
     args = _args()
--- utils/dataloader.py
+++ utils/dataloader.py
@@ -10,12 +10,9 @@ from PIL import Image
 from utils.custom_transforms import *
 
 class PolypDataset(data.Dataset):
-    """
-    dataloader for polyp segmentation tasks
-    """
     def __init__(self, root, transform_list):
         image_root, gt_root = os.path.join(root, 'images'), os.path.join(root, 'masks')
-        
+
         self.images = [os.path.join(image_root, f) for f in os.listdir(image_root) if f.endswith('.jpg') or f.endswith('.png')]
         self.images = sorted(self.images)
         
@@ -26,6 +23,7 @@ class PolypDataset(data.Dataset):
         
         self.size = len(self.images)
         self.transform = self.get_transform(transform_list)
+
     @staticmethod
     def get_transform(transform_list):
         tfs = []
@@ -38,42 +36,27 @@ class PolypDataset(data.Dataset):
         return transforms.Compose(tfs)
 
     def __getitem__(self, index):
-        image = self.rgb_loader(self.images[index])
-        gt = self.binary_loader(self.gts[index])
-        
-        original_size = gt.size[::-1]
+        image = Image.open(self.images[index]).convert('RGB')
+        gt = Image.open(self.gts[index]).convert('L')
+        shape = gt.size[::-1]
         name = self.images[index].split('/')[-1]
         if name.endswith('.jpg'):
             name = name.split('.jpg')[0] + '.png'
+            
+        sample = {'image': image, 'gt': gt, 'name': name, 'shape': shape}
 
-        sample = self.transform({'image': image, 'gt': gt})
-        sample['name'] = name
-        sample['original_size'] = original_size
+        sample = self.transform(sample)
         return sample
 
     def filter_files(self):
         assert len(self.images) == len(self.gts)
-        images = []
-        gts = []
+        images, gts = [], []
         for img_path, gt_path in zip(self.images, self.gts):
-            img = Image.open(img_path)
-            gt = Image.open(gt_path)
+            img, gt = Image.open(img_path), Image.open(gt_path)
             if img.size == gt.size:
                 images.append(img_path)
                 gts.append(gt_path)
-        self.images = images
-        self.gts = gts
-
-    def rgb_loader(self, path):
-        with open(path, 'rb') as f:
-            img = Image.open(f)
-            return img.convert('RGB')
-
-    def binary_loader(self, path):
-        with open(path, 'rb') as f:
-            img = Image.open(f)
-            # return img.convert('1')
-            return img.convert('L')
+        self.images, self.gts = images, gts
 
     def __len__(self):
         return self.size
